50cca885a78b01ba147fe093457119cec93f6028
jungmin <justin930522@gmail.com>
2023-07-15 23:15:04 +0900

    feat:cors진행중

--- app/api/governance/route.ts
+++ app/api/governance/route.ts
@@ -1,10 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server'
 const db = require("../../../common/config/db")
 
+export const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+
 let queryString;
 
-const queryPromise = (queryString: string) => {
-	return new Promise((resolve, reject) => {  
+
+const queryPromise = (queryString: string, req:NextRequest, res:NextResponse) => {
+	return new Promise((resolve, reject) => {
 		db.query(queryString, (error: any, results: any) => {  
 			if (error) {  
 				return reject(error);  
@@ -17,10 +25,10 @@ const queryPromise = (queryString: string) => {
 export const GET = async (req:NextRequest, res:NextResponse) => {
   try{
     queryString = `SELECT * FROM governance ORDER BY id DESC`  
-    const rows = await queryPromise(queryString)
-    return NextResponse.json(rows)
+    const rows = await queryPromise(queryString, req, res)
+    return NextResponse.json(rows, {headers:corsHeaders})
   } catch(e){
-
+    console.error(e)
   }
 }
 
@@ -30,7 +38,7 @@ export const POST = async (req:NextRequest) => {
     console.log(res)
     const query = db.query(`INSERT INTO governance (subject, content) VALUES ("${res.subject}", "${res.content}")`)
     console.log(query)
-    return NextResponse.json(res)
+    return NextResponse.json(res, {headers:corsHeaders})
   }catch(e){
     console.error(e);
   }
