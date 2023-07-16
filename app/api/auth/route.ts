2b3bea1a9566ae1c233094c6247b5ab6a63977c3
jungmin <justin930522@gmail.com>
2023-07-16 15:27:30 +0900

    feat:점검중

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -4,12 +4,18 @@ const data = {
   "ddd": "dkdkdk"
 }
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
 export const GET = async (request: NextRequest) => {
   //fetch
   try{
   
     return new NextResponse(JSON.stringify(data)); 
   }catch(err){
-    return new NextResponse("Database Error", {status: 500});
+    return new NextResponse("Database Error", {headers:corsHeaders});
   }
 }
