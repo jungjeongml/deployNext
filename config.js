c31e7712b11606e256dd50f63c006d0d0bc80a91
jungmin <justin930522@gmail.com>
2023-07-18 16:50:13 +0900

    fix:request수정

--- config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  PT:'http',
-  HOST:'localhost',
-  PORT:3000,
-}
-
-export default config
--- request.ts
+++ request.ts
@@ -1,9 +1,11 @@
-import axios from 'axios'
-import config from './config'
 require('dotenv').config();
 
+import axios from 'axios'
+import config from './config'
+
+console.log(process.env.DEVURL)
 const request = axios.create({
-  baseURL: `https://jungml.shop`,
+  baseURL: process.env.DEVURL,
   headers: {
     "Content-Type":"application/json"
   }
